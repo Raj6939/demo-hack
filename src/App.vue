74bc37a12d87dae7ed7b779cd33b4436945673c7
raj6939 <raj.v.patil108@gmail.com>
2023-05-30 14:22:15 +0530

    initial UI

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <HelloWorld v-for="(card, index) in cards" :key="index" :card="card"/>
+    
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import project from "../projects.json"
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      cards:project
+      // cards: [
+      //   { id: 1, title: 'Card 1', description: 'Description for Card 1' },
+      //   { id: 2, title: 'Card 2', description: 'Description for Card 2' },
+      //   { id: 3, title: 'Card 3', description: 'Description for Card 3' },
+      //   { id: 4, title: 'Card 3', description: 'Description for Card 3' },
+      //   { id: 5, title: 'Card 3', description: 'Description for Card 3' },
+      //   { id: 6, title: 'Card 3', description: 'Description for Card 3' }
+      // ]
+    }
+    }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,74 @@
+<template>
+   <div class="text-center">
+    <b-card
+      :title="card.title"
+      img-src="https://picsum.photos/600/300/?image=25"
+      img-alt="Image"
+      img-top
+      tag="article"
+      style="max-width: 20rem;"
+      class="mb-2 mx-auto"
+    >
+      <b-card-text>
+        {{ card.description }}
+      </b-card-text>
+      <b-button :href="card.projectLink" target="_blank" variant="primary" >View</b-button>  
+      <footer>
+        <small>
+          <b-badge
+          v-for="(tag, index) in card.tags" :key="index" :card="card"
+                  pill variant="secondary" style="margin-left: 2px">{{tag}}</b-badge>
+        </small>
+      </footer>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: ['card']  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+
+.text-center {
+  text-align: center;
+  display: inline-block;
+  margin: 10px;
+}
+
+.mb-2 {
+  margin-bottom: 2rem;
+}
+
+.mx-auto {
+  margin-left: auto;
+  margin-right: auto;
+}
+
+@media (max-width: 768px) {
+  .text-center {
+    text-align: left;
+  }
+}
+</style>
