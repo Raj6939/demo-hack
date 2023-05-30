46d14a993c20c97d92a4096beb2ba965ffe298af
raj6939 <raj.v.patil108@gmail.com>
2023-05-30 23:32:46 +0530

    updated json and yml

--- generate-index-json.js
+++ generate-index-json.js
@@ -1,33 +1,40 @@
 const fs = require('fs');
 const path = require('path');
 
-const methodsDir = path.join(__dirname, './allProjects');
+// set directories and paths
+const methodsDir = path.join(__dirname, './methods');
 const indexFile = path.join(methodsDir, 'index.json');
 
-const allMethods = [];
-process.stdout.write('Processing:');
-fs.readdirSync(methodsDir).forEach((filename) => {
-  if (filename === 'index.json') {
-    return;
-  }
+// Process all method files and generate an index file
+(async () => {
 
-  const methodFile = path.join(methodsDir, filename);
-  const methodData = fs.readFileSync(methodFile, 'utf-8');
-  process.stdout.write(' ' + filename);
+  const allMethods = [];
+  process.stdout.write('Processing:');
+  fs.readdirSync(methodsDir).forEach(filename => {
+    // skip index file
+    if(filename === 'index.json') {
+      return;
+    }
 
-  try {
-    const methodJson = JSON.parse(methodData);
-    allMethods.push(methodJson);
-  } catch (e) {
-    console.error('\n\n❌ ERROR: Failed to parse', filename, e);
+    const methodFile = path.join(methodsDir, filename);
+    const methodData = fs.readFileSync(methodFile, 'utf-8');
+    process.stdout.write(' ' + filename);
+
+    try {
+      const methodJson = JSON.parse(methodData);
+      allMethods.push(methodJson);
+    } catch(e) {
+        console.error('\n\n❌ ERROR: Failed to parse', filename, e);
+        process.exit(1);
+    }
+  });
+
+  if(allMethods.length > 0) {
+    fs.writeFileSync(indexFile, JSON.stringify(allMethods, null, 2), 'utf-8');
+    console.log('\n\nGenerated /methods/index.json.');
+  } else {
+    console.error('\n\n❌ ERROR: No methods found in methods directory');
     process.exit(1);
   }
-});
 
-if (allMethods.length > 0) {
-  fs.writeFileSync(indexFile, JSON.stringify(allMethods, null, 2), 'utf-8');
-  console.log('\n\nGenerated /methods/index.json.');
-} else {
-  console.error('\n\n❌ ERROR: No methods found in methods directory');
-  process.exit(1);
-}
+})();
