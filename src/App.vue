a7547eb3030d4ff6b94849e72722b8603dae6f47
raj6939 <raj.v.patil108@gmail.com>
2023-05-31 15:20:04 +0530

    design added

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,39 @@
 <template>
-  <div id="app" class="text-center">  
-    <p>Projects built using Hypersign</p>  
-      <b-card
-        v-for="(data, index) in jsonData"
-        :key="index"
-        :title="data.name"
-        img-src="https://picsum.photos/600/300/?image=25"
-        img-alt="Image"
-        img-top
-        tag="article"
-        style="max-width: 20rem; padding:10px;"
-        class="mb-2"
-      >
-        <b-card-text>
-          {{ data.description }}
-        </b-card-text>
-        <b-button :href="data.projectLink" target="_blank" variant="primary">View</b-button>
-        <footer>
-          <small>
-            <b-badge
-              v-for="(tag, index) in data.tags"
-              :key="index"
-              :data="data"
-              pill
-              variant="secondary"
-              style="margin-left: 2px"
-            >{{ tag }}</b-badge>
-          </small>
-        </footer>
-      </b-card>    
+  <div id="app" class="d-flex justify-content-center flex-column">  
+    <h1 class="text-center">Projects built using Hypersign</h1>  
+    <div class="container">
+  <div class="row d-flex justify-content-center">
+    <b-card
+      v-for="(data, index) in jsonData"
+      :key="index"
+      :title="data.name"
+      img-src="https://picsum.photos/600/300/?image=25"
+      img-alt="Image"
+      img-top
+      tag="article"
+      style="max-width: 20rem; padding:10px;"
+      class="text-center col-md-3 mb-2"
+    >
+      <b-card-text>
+        {{ data.description }}
+      </b-card-text>
+      <b-button :href="data.projectLink" target="_blank" variant="primary">View</b-button>
+      <footer>
+        <small>
+          <b-badge
+            v-for="(tag, index) in data.tags"
+            :key="index"
+            :data="data"
+            pill
+            variant="secondary"
+            style="margin-left: 2px"
+          >{{ tag }}</b-badge>
+        </small>
+      </footer>
+    </b-card>
+  </div>
+</div>
+
   </div>
 </template>
 
